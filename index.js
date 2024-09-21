da01be658848de3a9f3f0818261e33db4cf28ee6
João Vitor Tupan Dias <joaovtupan@gmail.com>
2024-09-21 18:19:54 -0300

    quarto commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ document.getElementById('contactForm').addEventListener('submit', function() {
     var nome = document.getElementById('nome').value;
     var endereco = document.getElementById('endereco').value;
     var mensagem = document.getElementById('mensagem').value;
-    var mensagemCompleta = `Olá, meu nome é ${nome}, moro no esndereço: ${endereco}. E gostaria de fazer um pedido: ${mensagem}.`;
+    var mensagemCompleta = `Olá, meu nome é ${nome}, moro no endereço: ${endereco}. E gostaria de fazer um pedido: ${mensagem}.`;
     var numeroWhatsApp = '554391165171'; 
     var linkWhatsApp = `https://wa.me/${554391165171}?text=${encodeURIComponent(mensagemCompleta)}`;
     
